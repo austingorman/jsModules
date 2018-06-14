cb9d3fbb7727343b3d7e56efaf9b7ee5bd5c78bd
Austin Gorman <austin.gorman@gmail.com>
2018-06-14 15:56:55 -0500

    first commit

---
+++ src/scripts/contact.js
@@ -0,0 +1,22 @@
+const people = {
+  John: {
+    phone: "838-939-9393",
+    address: "123 Dog Street",
+  },
+  Himasf: {
+    phone: "838-929-9222",
+    address: "112 Dog Street",
+  },
+  Bread: {
+    phone: "234-233-2342",
+    address: "938 Hurh Lane"
+  },
+  Dracula: {
+    phone: "293-030-9338",
+    address: "839 Brean Drive"
+  },
+  Hivey: {
+    phone: "929-912-9090",
+    address: "839 Dad Road"
+  }
+}
